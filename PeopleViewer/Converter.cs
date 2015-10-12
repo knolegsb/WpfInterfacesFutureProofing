244e563783c4338a44f8763d98dfdcb02bfe61fb
knolegsb <seon2you@hotmail.com>
2015-10-11 21:39:27 -0700

    start

---
+++ PeopleViewer/Converter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PeopleViewer
+{
+    public class DecadeConverter : IValueConverter 
+    {
+        public object Convert(object value, Type tagetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+
+        }
+    }
+}
