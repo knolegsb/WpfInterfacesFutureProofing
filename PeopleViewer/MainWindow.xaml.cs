faf9f5e1faeaeb40b10447272f420a001e96e40d
knolegsb <seon2you@hotmail.com>
2015-10-11 22:25:36 -0700

    solved errors

--- PeopleViewer/MainWindow.xaml.cs
+++ PeopleViewer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PersonLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,43 @@ namespace PeopleViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        PeopleRepository peopleRepo = new PeopleRepository();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ConcreteFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+
+            List<Person> people;
+            people = peopleRepo.GetPeople();
+            PersonListBox.ItemsSource = people;
+            //foreach (var person in people)
+            //    PersonListBox.Items.Add(person);
+        }
+
+        private void InterfaceFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+
+            IEnumerable<Person> people;
+            people = peopleRepo.GetPeople();
+            PersonListBox.ItemsSource = people;
+            //foreach (var person in people)
+            //    PersonListBox.Items.Add(person);
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+        }
+
+        private void ClearListBox()
+        {
+            //PersonListBox.Items.Clear();
+            PersonListBox.ItemsSource = null;
         }
     }
 }
